1eec0b49a784da559364327eb95b94f570a47146
Matheus J. Vieira Lemes <matheus.vieira@viamondo.com.br>
2018-01-19 09:58:05 -0200

    Criação das páginas e navegação entre elas

--- routes.js
+++ routes.js
@@ -21,4 +21,57 @@ module.exports = function (app) {
             res.sendFile(path + 'login.html')
         })
 
+    app.route('/downloads')
+        .get(function (req,res
+        ){ 
+            res.sendFile(path + 'downloads.html')
+        })
+
+    app.route('/manuaisvendas')
+        .get(function (req,res
+        ){ 
+            res.sendFile(path + 'manuaisvendas.html')
+        })
+    
+    app.route('/manuaispv')
+        .get(function (req,res
+        ){ 
+            res.sendFile(path + 'manuaispv.html')
+        })
+    
+    app.route('/manuaisadm')
+        .get(function (req,res
+        ){ 
+            res.sendFile(path + 'manuaisadm.html')
+        })
+    
+    app.route('/manuaisest')
+        .get(function (req,res
+        ){ 
+            res.sendFile(path + 'manuaisest.html')
+        })
+    
+    app.route('/manuaiscomp')
+        .get(function (req,res
+        ){ 
+            res.sendFile(path + 'manuaiscomp.html')
+        })
+    
+    app.route('/manuaisfei')
+        .get(function (req,res
+        ){ 
+            res.sendFile(path + 'manuaisfei.html')
+        })
+    
+    app.route('/manuaistrein')
+        .get(function (req,res
+        ){ 
+            res.sendFile(path + 'manuaistrein.html')
+        })
+    app.route('/vagas')
+        .get(function (req,res
+        ){ 
+            res.sendFile(path + 'vagas.html')
+        })
+        
 }
